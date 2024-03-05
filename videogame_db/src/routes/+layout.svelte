5196b164d483637fd9f9e2fb31be2084875fdd36
jerelSantos <jerel-tsar.santos@wsu.edu>
2024-03-05 14:34:02 -0800

    Added flowbite library, added routes, added first version of navbar

---
+++ videogame_db/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import '../app.pcss';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+
+<Navbar let:NavContainer color="primary">
+	<NavContainer class="border px-5 py-2 rounded-lg bg-white dark:bg-gray-600">
+	  <NavBrand href="/">
+		<img src="../../static/favicon.png" class="me-3 h-6 sm:h-9" alt="Logo" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold">Video Game DB</span>
+	  </NavBrand>
+	  <NavHamburger />
+	  <NavUl>
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="/profile">Profile</NavLi>
+		<NavLi href="/signup">Sign Up</NavLi>
+		<NavLi href="/login">Login</NavLi>
+	  </NavUl>
+	</NavContainer>
+</Navbar>
+
+<slot />
