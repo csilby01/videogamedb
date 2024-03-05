3eb52a126a8230999e24d87e451764637c4879b0
jerelSantos <jerel-tsar.santos@wsu.edu>
2024-03-05 15:33:20 -0800

    navbar update; tailwind stuff

--- videogame_db/src/routes/+page.svelte
+++ videogame_db/src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <!-- Homepage -->
 <script>
-    
+    import {Skeleton, ImagePlaceholder, TextPlaceholder} from 'flowbite-svelte';
+    import Navbar from './Navbar.svelte';
 </script>
 
-<h1>Homepage</h1>
+<Navbar/>
+
+<div class="flex flex-row justify-center items-center">
+   <h1 class="">Video Game DB</h1>
+</div>
