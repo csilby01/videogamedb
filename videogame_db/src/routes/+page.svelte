ced963eb9c0b824b57ec54203e762e14bdb0bdf9
jerelSantos <jerel-tsar.santos@wsu.edu>
2024-03-05 19:44:54 -0800

    added 'popular games' carousel

--- videogame_db/src/routes/+page.svelte
+++ videogame_db/src/routes/+page.svelte
@@ -1,11 +1,53 @@
 <!-- Homepage -->
 <script>
-    import {Skeleton, ImagePlaceholder, TextPlaceholder} from 'flowbite-svelte';
     import Navbar from './Navbar.svelte';
+    import { Carousel } from 'flowbite-svelte';
+    import { scale } from 'svelte/transition';
+    import {quintOut} from 'svelte/easing';
+    import game1 from '$lib/images/game1_placeholder.png';
+    import game2 from '$lib/images/game2_placeholder.png';
+    import game3 from '$lib/images/game3_placeholder.png';
+
+    const images = [
+        {
+            alt: 'game 1',
+            src: game1,
+            title: 'game1'
+        },
+        {
+            alt: 'game 2',
+            src: game2,
+            title: 'game2'
+        },
+        {
+            alt: 'game 2',
+            src: game3,
+            title: 'game2'
+        }
+    ]
+
+    const scaleAnimation = (x) => scale (x, {duration: 500, easing: quintOut });
 </script>
 
 <Navbar/>
 
-<div class="flex flex-row justify-center items-center">
-   <h1 class="">Video Game DB</h1>
+<div class="flex flex-row justify-center items-center bg-sky-800 mx-28 py-9">
+   <h1 class="text-5xl">Video Game DB</h1>
+</div>
+
+<div class="grid grid-cols-5 gap-2 my-10">
+    <div class='col-span-4'>
+        <div class="flex flex-row justify-center items-center mx-10">
+            <h1>Popular Games</h1>
+        </div>
+        <div class="max-w-4x1 mx-24">
+            <Carousel {images} transition={scaleAnimation} let:Indicators let:Controls>
+                <Controls />
+                <Indicators />
+            </Carousel>
+        </div>
+    </div>
+    <div class="bg-sky-800 content-center">
+        <h1 class="text 4xl">Column 2</h1>
+    </div>
 </div>
--- videogame_db/src/routes/Navbar.svelte
+++ videogame_db/src/routes/Navbar.svelte
@@ -2,12 +2,13 @@
 	import '../app.pcss';
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input } from 'flowbite-svelte';
     import { SearchOutline } from 'flowbite-svelte-icons';
+    import logo from "$lib/images/logo.png";
 </script>
 
 <Navbar>
     <NavBrand href="/">
-      <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
-      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+      <img src={logo} class="me-3 h-6 sm:h-9" alt="Video Game DB Logo" />
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Video Game DB</span>
     </NavBrand>
     <div class="flex md:order-2">
       <Button color="none" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
