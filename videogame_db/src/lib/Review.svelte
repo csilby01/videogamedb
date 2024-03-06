74d041ad3e0333058b6ff6fe41c599c2ec1e43be
jerelSantos <jerel-tsar.santos@wsu.edu>
2024-03-05 22:21:29 -0800

    Added recent reviews outline to homepage

---
+++ videogame_db/src/lib/Review.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { Avatar, P, Skeleton, TextPlaceholder } from 'flowbite-svelte';
+    import pfp from '$lib/images/pfp_placeholder.jpg';
+    import game1 from '$lib/images/game1_placeholder.png';
+</script>
+
+<div>
+    <div class="flex items-center space-x-4 rtl:space-x-reverse">
+        <Avatar src={pfp} />
+        <div>
+            <div>User 1</div>
+            <div>Played Game 1</div>
+        </div>
+    </div>
+    <div class="grid grid-cols-5">
+        <img src={game1} alt="game1" class="col-span-2"/>
+        <!-- <P align="center" class="col-span-3">
+            Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. 
+        </P> -->
+        <div class="col-span-2 mx-4">
+            <TextPlaceholder size="md"/>
+            <TextPlaceholder size="xl mt-3"/>
+        </div>
+    </div>
+</div>
--- videogame_db/src/routes/+page.svelte
+++ videogame_db/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <!-- Homepage -->
 <script>
-    import Navbar from './Navbar.svelte';
+    import Navbar from '../lib/Navbar.svelte';
+    import Review from '../lib/Review.svelte';
     import { Carousel } from 'flowbite-svelte';
     import { scale } from 'svelte/transition';
     import {quintOut} from 'svelte/easing';
@@ -46,8 +47,33 @@
                 <Indicators />
             </Carousel>
         </div>
+        <div class="flex flex-row justify-center items-center mx-10 mt-5">
+            <h1>New Releases</h1>
+        </div>
+        <div class="max-w-4x1 mx-24">
+            <Carousel {images} transition={scaleAnimation} let:Indicators let:Controls>
+                <Controls />
+                <Indicators />
+            </Carousel>
+        </div>             
     </div>
-    <div class="bg-sky-800 content-center">
-        <h1 class="text 4xl">Column 2</h1>
+    <div class="bg-sky-800 items-center justify-center">
+        <h1 class="text-2xl text-center">Top 3 Games of the Month</h1>
+        <div class='border-b-2 border-sky-500 mx-5 pt-3'>
+            1. Game 1<img src={game1} alt="game 1" class='pb-2'/>
+        </div>
+        <div class='border-b-2 border-sky-500 mx-5 pt-3'>
+            2. Game 2<img src={game2} alt="game 1" class='pb-2'/>
+        </div>
+        <div class='border-b-2 border-sky-500 mx-5 pt-3'>
+            3. Game 3<img src={game3} alt="game 1" class='pb-2'/>
+        </div>
     </div>
+</div>
+
+<div class= "mx-24 my-5">
+    <h1>Recent Reviews: </h1>
+    <Review />
+    <Review />
+    <Review />
 </div>
