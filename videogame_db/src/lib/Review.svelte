6021b02872d9112263adf27b2aa73d09720fee25
jerelSantos <jerel-tsar.santos@wsu.edu>
2024-03-06 18:15:24 -0800

    Added game page mockup

--- videogame_db/src/lib/Review.svelte
+++ videogame_db/src/lib/Review.svelte
@@ -4,7 +4,7 @@
     import game1 from '$lib/images/game1_placeholder.png';
 </script>
 
-<div class="border border-gray-500 my-3 p-5">
+<div class="border border-gray-500 my-3 pr-5">
     <div class="flex items-center space-x-4 rtl:space-x-reverse">
         <Avatar src={pfp} />
         <div>
@@ -12,10 +12,10 @@
         </div>
     </div>
     <div class="grid grid-cols-5">
-        <img src={game1} alt="game1" class="col-span-2"/>
-        <!-- <P align="center" class="col-span-3">
-            Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. Review text. 
-        </P> -->
+        <div class="col-span-2">
+            <img src={game1} alt="game1"/>
+            <h1>4 Stars</h1>
+        </div>
         <div class="col-span-2 mx-4">
             <TextPlaceholder size="md"/>
             <TextPlaceholder size="xl mt-3"/>
---
+++ videogame_db/src/routes/game/+page.svelte
@@ -0,0 +1,41 @@
+<!-- Game Page -->
+<script>
+	import Navbar from "../../lib/Navbar.svelte";
+    import game1 from '$lib/images/game1_placeholder.png';
+	import BarGraph from "../../lib/BarGraph.svelte";
+    import { TextPlaceholder  } from "flowbite-svelte";
+	import Review from "../../lib/Review.svelte";
+</script>
+
+<Navbar />
+
+<div class="grid grid-cols-7">
+    <div class="col-span-2 m-2">
+        <img src={game1} alt="game 1" class='pb-2'/>
+        <h1>Date • Publisher</h1>
+        <h1>Genre</h1>
+        <h1>Console(s)</h1>
+        <h1>Tags</h1>
+    </div>
+    <div class="col-span-3 m-2">
+        <h1 class="text-5xl text-center whitespace-nowrap font-semibold dark:text-white">Game 1</h1>
+        <h1>Summary</h1>
+        <TextPlaceholder size="md"/>
+        <TextPlaceholder size="xl mt-3"/>
+    </div>
+    <div class="col-span-2 bg-sky-800 items-center justify-center pb-1 m-2">
+        <h1 class= "text-2xl text-center font-semibold dark:text-white:">Avg Rating: 3.54 Stars</h1>
+        <BarGraph />
+    </div>
+</div>
+
+<div class="grid grid-cols-7">
+    <div class="col-span-2 m-2"></div>
+
+    <div class="col-span-5 m-2">
+        <h1 class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Recent Reviews: </h1>
+        <Review />
+        <Review />
+        <Review />
+    </div>
+</div>
