5fce0490b37d5f055c43fbe176a483097757fdad
jsantos1 <jerel-tsar.santos@wsu.edu>
2024-03-11 10:11:57 -0700

    redid sizing and scaling for review component

--- videogame_db/src/lib/Review.svelte
+++ videogame_db/src/lib/Review.svelte
@@ -4,21 +4,56 @@
     import game1 from '$lib/images/game1_placeholder.png';
 </script>
 
-<div class="border border-gray-500 my-3 p-3">
-    <div class="flex items-center space-x-4 rtl:space-x-reverse">
-        <Avatar src={pfp} />
-        <div>
-            <P><strong>User</strong> played <strong>Game 1</strong></P>
-        </div>
+<div class="review_layout border border-gray-500 my-3 p-3">
+    <div class="col_1">
+        <h1 class="row-span-1">Game 1</h1>
+        <a href="/game"><img src={game1} alt="game1" class="row-span-1"/></a>
     </div>
-    <div class="grid grid-cols-5">
-        <div class="col-span-2">
-            <img src={game1} alt="game1"/>
-            <h1>4 Stars</h1>
+    <div class="col_2 mx-3">
+        <div class="row-span-1">
+            <div class="flex items-center space-x-4 rtl:space-x-reverse">
+                <Avatar src={pfp} />
+                <div class="space-y-1">
+                    <div><P><strong>User</strong> played <strong>Game 1</strong></P></div>
+                </div>
+            </div>
         </div>
-        <div class="col-span-2 mx-4">
+        <div class="row-span-1">
             <TextPlaceholder size="md"/>
             <TextPlaceholder size="xl mt-3"/>
         </div>
     </div>
+    <div class="col_3">
+        Rating
+    </div>
 </div>
+
+<style>
+    .review_layout{
+        display: grid;
+        grid-template-columns: minmax(100px, 1fr) minmax(400px, 4fr) minmax(50px, 1fr);
+    }
+    .col_1 {
+        display:grid;
+        grid-template-rows: minmax(30px, 1fr) minmax(100px, 2fr)
+    }
+    .col_1 > :first-child {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .col_2 {
+        display: grid;
+        grid-template-rows: 1fr 4fr
+    }
+    .col_2 > :first-child {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .col_3 {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
