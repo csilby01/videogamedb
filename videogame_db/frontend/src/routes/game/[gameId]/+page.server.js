3a142dec91d16a9343510f3d750ae6bbda6e1c67
jerelSantos <jerel-tsar.santos@wsu.edu>
2024-04-17 15:43:24 -0700

    Fixed game fetching?

--- videogame_db/frontend/src/routes/game/[gameId]/+page.server.js
+++ videogame_db/frontend/src/routes/game/[gameId]/+page.server.js
@@ -1,12 +1,13 @@
 import { sequelize } from '/src/lib/db/db.js';
 import { getGameCover } from '../../../lib/db/utils/getCover';
+import Game from '$lib/models/Game.js'
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
     const { gameId } = params;
     console.log("Fetching game with ID:", gameId);
     try {
-        const game = await sequelize.models.Game.findByPk(gameId);
+        const game = await Game.findByPk(gameId);
         console.log('Found Game:', game.title);
 
         const coverURL = await getGameCover(game.game_photo);
