af351b0b2e9b4935041a2c5ce5144f66148c3cbb
jerelSantos <jerel-tsar.santos@wsu.edu>
2024-04-19 00:26:09 -0700

    db

--- videogame_db/frontend/src/initDB.js
+++ videogame_db/frontend/src/initDB.js
@@ -1,10 +1,21 @@
 import { sequelize } from './lib/db/db.js';
 import './lib/db/initModels.js'; // This ensures models are loaded and associated
+import Reviews from './lib/models/Reviews.js';
+import Game from './lib/models/Game.js'
 
 async function startApp() {
   try {
     await sequelize.sync();
     // const queryInterface = sequelize.getQueryInterface();
+    Game.hasMany(Reviews, {
+      foreignKey: 'review_id',
+      as: 'reviews'
+    });
+    Reviews.belongsTo(Game, {
+      foreignKey: 'game_id',
+      as: 'game'
+    });
+    console.log(Game.associations);
     console.log('Database synced');
   } catch (error) {
     console.error('Unable to sync database:', error);
